eb10709be04c37b628ca5bacaba6c85b72b9b7d4
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-11-19 14:32:51 +0300

    Добавляем сущности БД. Связь между ними ManyToMany (может быть много книг у автора и также может быть несколько авторов у книги)
    Заготовки для репозитариев к сущностям.
    
    Начальную миграцию для создания таблиц БД.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # some-example
+в hosts прописать 
+127.0.0.1 example.loc
+
+Переходим в папку docker 
+
+docker-compose build
+
+База db_simple должна будет создаться автоматически
+
+docker-compose up (для запуска контейнеров)
+
+переходим в контейнер с вебом 
+
+make jumpin
+
+Там инициируем миграции
+
+1 Создаем нужные таблицы 
+   php bin/console doctrine:migrations:migrate
+   
+2 Заполняем фикстуры
+  
+
---
+++ src/Entity/Author.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ */
+class Author
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $middleName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $lastName;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Book::class, mappedBy="authors")
+     */
+    private ArrayCollection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $name): self
+    {
+        $this->firstName = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMiddleName(): string
+    {
+        return $this->middleName;
+    }
+
+    public function setMiddleName(string $middleName): self
+    {
+        $this->middleName = $middleName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Book[]
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): self
+    {
+        if (!$this->books->contains($book)) {
+            $this->books[] = $book;
+            $book->addAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): self
+    {
+        if ($this->books->removeElement($book)) {
+            $book->removeAuthor($this);
+        }
+
+        return $this;
+    }
+
+}
